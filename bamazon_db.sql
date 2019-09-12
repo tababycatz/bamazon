dd3c1b34f1b519edd9375bb8ecfd57f1f9d2d440
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-12 11:12:13 -0400

    update sql text for all groceries

--- bamazon_db.sql
+++ bamazon_db.sql
@@ -4,12 +4,40 @@ CREATE database bamazon_db;
 USE bamazon_db;
 
 CREATE TABLE products (
-  item_id INT NOT NULL,
+  item_id INT NOT NULL AUTO_INCREMENT,
   product_name VARCHAR(100) NULL,
   department_name VARCHAR(100) NULL,
   stock_quantity INT NULL,
-  price DECIMAL(10,4) NULL,
+  price DECIMAL(10,2) NULL,
   PRIMARY KEY (item_id)
 );
 
-SELECT * FROM products;
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("apple", "grocery" ,100 ,1.00);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("milk", "grocery" ,50 ,3.99);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("chicken breast", "grocery" ,50 ,2.99);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("flank steak", "grocery" ,50 ,4.00);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("dozen of eggs", "grocery" ,60 ,2.99);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("brioche bread", "grocery" ,20 ,1.99);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("chocolate chip cookies", "grocery" ,50 ,3.50);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("coca cola", "grocery" ,100 ,1.99);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("kale", "grocery" ,100 ,2.00);
+
+INSERT INTO products (product_name , department_name, stock_quantity, price)
+VALUES ("garlic", "grocery" ,150 ,0.99);
