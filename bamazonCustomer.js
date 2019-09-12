e4ebef2411d084f7efe112322fdb87664326d443
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-12 14:27:31 -0400

    updated with packages installed

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -15,101 +15,39 @@ var connection = mysql.createConnection({
   database: "bamazon_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   selectPurchase();
 });
 
-fucntion selectPurchase() {
-    inquirer
-    .prompt({
-        name: "action",
-        type: "list",
-        message: "Welcome to BAmazon, what would you like to purchase?",
-        choices: [
-            "Apple",
-            "Milk",
-            "Chicken Breast",
-            "Flank Steak",
-            "Eggs",
-            "Brioche Bread",
-            "Chocolate Chip Cookies",
-            "Coca Cola (12 cans)",
-            "Kale",
-            "Garlic"
-        ]
-    })
-}
-
-fucntion howMany() {
-  inquirer
-  .prompt({
-      name: "value",
-      type: "input",
-      message: "How many would you like to purchase?",
-      choices: [
-          "Apple",
-          "Milk",
-          "Chicken Breast",
-          "Flank Steak",
-          "Eggs",
-          "Brioche Bread",
-          "Chocolate Chip Cookies",
-          "Coca Cola (12 cans)",
-          "Kale",
-          "Garlic"
-      ]
+function selectPurchase() {
+inquirer
+  .prompt([{
+    name: "value",
+    type: "input",
+    message: "Welcome to BAmazon, what would you like to purchase?",
+    choices: [
+      "Apple",
+      "Milk",
+      "Chicken Breast",
+      "Flank Steak",
+      "Eggs",
+      "Brioche Bread",
+      "Chocolate Chip Cookies",
+      "Coca Cola (12 cans)",
+      "Kale",
+      "Garlic"
+    ]
+  },
+  {
+    name: "value2",
+    type: "input",
+    message: "How many would you like to purchase?"
+  }])
+  .then(function (answer) {
+    console.log(answer.value)
+    console.log(answer.value2)
+    var newvalue = parseInt(answer.value) + parseInt(answer.value2);
+    console.log(newvalue)
   })
-.then(function(answer) {
-  switch (answer.action) {
-  case "":
-    updateInventory();
-    break;
-
-  case "How many would you like to purchase?":
-    multiSearch();
-    break;
-
-  case "Find data within a specific range":
-    rangeSearch();
-    break;
-
-  case "Search for a specific song":
-    songSearch();
-    break;
-
-  case "exit":
-    connection.end();
-    break;
- } 
-});
-
-function updateInventory() {
-  var query = connection.query(
-    "UPDATE products";
-  
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + " inventory updated!\n");
-      // Call deleteProduct AFTER the UPDATE completes
-      deleteProduct();
-    }
-  );
-
-  // logs the actual query being run
-  console.log(query.sql);
-}
-
-function purchasedProduct() {
-  console.log("You have purchased...");
-  connection.query(
-    "DELETE FROM products";
-
-    function(err, res) {
-      if (err) throw err;
-      console.log(res.affectedRows + "you have sucessfully purchased!\n");
-      // Call readProducts AFTER the DELETE completes
-      readProducts();
-    }
-  );
 }
