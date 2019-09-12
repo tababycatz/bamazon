897b48972e0a444309a8ef8b4b17cfafbbd09d99
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-12 13:48:27 -0400

    update with package.json

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -39,27 +39,77 @@ fucntion selectPurchase() {
             "Garlic"
         ]
     })
-    .then(function(answer) {
-        switch (answer.action) {
-        case "Find songs by artist":
-          artistSearch();
-          break;
+}
+
+fucntion howMany() {
+  inquirer
+  .prompt({
+      name: "value",
+      type: "input",
+      message: "How many would you like to purchase?",
+      choices: [
+          "Apple",
+          "Milk",
+          "Chicken Breast",
+          "Flank Steak",
+          "Eggs",
+          "Brioche Bread",
+          "Chocolate Chip Cookies",
+          "Coca Cola (12 cans)",
+          "Kale",
+          "Garlic"
+      ]
+  })
+.then(function(answer) {
+  switch (answer.action) {
+  case "":
+    updateInventory();
+    break;
+
+  case "How many would you like to purchase?":
+    multiSearch();
+    break;
+
+  case "Find data within a specific range":
+    rangeSearch();
+    break;
+
+  case "Search for a specific song":
+    songSearch();
+    break;
+
+  case "exit":
+    connection.end();
+    break;
+ } 
+});
+
+function updateInventory() {
+  var query = connection.query(
+    "UPDATE products";
   
-        case "Find all artists who appear more than once":
-          multiSearch();
-          break;
-  
-        case "Find data within a specific range":
-          rangeSearch();
-          break;
-  
-        case "Search for a specific song":
-          songSearch();
-          break;
-  
-        case "exit":
-          connection.end();
-          break;
-        }
-      });
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + " inventory updated!\n");
+      // Call deleteProduct AFTER the UPDATE completes
+      deleteProduct();
+    }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+function purchasedProduct() {
+  console.log("You have purchased...");
+  connection.query(
+    "DELETE FROM products";
+
+    function(err, res) {
+      if (err) throw err;
+      console.log(res.affectedRows + "you have sucessfully purchased!\n");
+      // Call readProducts AFTER the DELETE completes
+      readProducts();
+    }
+  );
 }
