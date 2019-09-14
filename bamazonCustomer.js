1a8468c5105d9f6c80497fdaa92a9e75faad6ef8
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-14 17:52:50 -0400

    finished product!

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,63 +1,90 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-
 var connection = mysql.createConnection({
   host: "localhost",
-
   // Your port; if not 3306
   port: 3306,
-
   // Your username
   user: "root",
-
   // Your password
   password: "Generale1605!",
   database: "bamazon_db"
 });
-
 connection.connect(function (err) {
   if (err) throw err;
   selectPurchase();
 });
 
 function selectPurchase() {
-  connection.query("SELECT * FROM products", function(err,res) {
-  inquirer
-    .prompt([{
-      name: "action",
-      type: "list",
-      message: "Welcome to BAmazon, what would you like to purchase?",
-      choices: [
-        "Apple",
-        "Milk",
-        "Chicken Breast",
-        "Flank Steak",
-        "Eggs",
-        "Brioche Bread",
-        "Chocolate Chip Cookies",
-        "Coca Cola (12 cans)",
-        "Kale",
-        "Garlic"
-      ]
-    },
-    {
-      name: "value",
-      type: "input",
-      message: "How many would you like to purchase?"
-    }])
-    .then(function (answer) {
-      console.log(answer.action);
-      console.log(answer.value); 
-      for(var i = 0; i < .length; i++) {
-        console.log(res[i])   
-      }
-      // if (answer.value )  
-    })
-})
+  // our first attempt to collect data from our database
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    inquirer
+      .prompt([{
+        name: "action",
+        type: "list",
+        message: "Welcome to BAmazon, what would you like to purchase?",
+        choices: [
+          "Apple",
+          "Milk",
+          "Chicken Breast",
+          "Flank Steak",
+          "Eggs",
+          "Brioche Bread",
+          "Chocolate Chip Cookies",
+          "Coca Cola (12 cans)",
+          "Kale",
+          "Garlic"
+        ]
+      },
+        {
+          name: "value",
+          type: "input",
+          message: "How many would you like to purchase?"
+        }])
+        .then(function(answer){
+          console.log(answer);
+          // customer choices
+          var chosenItemName = answer.action.toLowerCase();
+          var chosenItemQuantity = answer.value;
+          // we can still 'res'(data/table) since we're inside of connection.query({..}) function from line 19 
+          // console.log(res);
+          // next we need to find the chosenItem(object) inside our products table(array) by looping over response from db
+          var chosenItem
+          for (var i = 0; i < res.length; i++) {
+            item = res[i];
+            // find the product based on the name of the item customer selected
+            if (item.product_name === chosenItemName) {
+              chosenItem = item
+            }
+          }
+
+          console.log(chosenItem)
+
+          // check if we have enough stock_quantity in our db
+          if (chosenItem.stock_quantity >= chosenItemQuantity) {
+            console.log(chosenItemQuantity + ' ' + chosenItem.product_name + ' sold!!!!!!!!!!!!!!!!!!!!!');
+
+            connection.query("UPDATE products SET stock_quantity=" + (res[i].stockquantity-answer.value) + "'WHERE productname='"+ product + "'", function(err,res){})
+          
+
+            // if we have enough apples(ie) in stock we should call another function next that would process the order and subtract the quantity from our stock_quantity in db
+            
+            // processOrder(chosenItem, chosenItemQuantity)
+            // - this function shoudl ping database to update afftected item's stock_quantity
+            
+          } else {
+            console.log('insufficient amount in stock, Please try to purchase a smaller amount of ' + chosenItem.product_name)
+            // redirect user to browse again since we cant process order
+            selectPurchase();
+          }
+
+
+        })
+  })
 }
 
 
-// This is to add up the purchases if you have more than just one type of inventory //
 // function confirmPurchase() {
 //   inquirer
 //     .prompt([{
@@ -72,19 +99,5 @@ function selectPurchase() {
 //       } else {
 //         selectPurchase()
 //       }
-// });
-
+//     })
 // }
-
-// for(cart.items.length)
-// function totalPurchase() {
-//   inquirer
-//     .prompt([{
-//       name: "action",
-//       type: "print",
-//       message: "Your total will be"
-//     }])
-//     .then(function (answer) {
-//       // connection.query()
-//     }
-
